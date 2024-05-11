4a4b6804be7958c31f60ffd496a161df76c7e4cd
danieldd57 <daniel.vargas19@uptc.edu.co>
2024-05-11 14:03:18 -0500

    creation of the menu for functions

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
-
 int main(void) {
-    printf("Hello, World!\n");
+    int option = 0;
+
+    while(option!=10){
+        printf("-----MENU----\n 4.Function #4 \n5.Function #5 \n6.Function #6 \n");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                printf("\n-- exit --");
+                break;
+        }
+    }
+
     return 0;
 }
+
