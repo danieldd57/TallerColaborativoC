9b98ca5a5353a6d808810fe4e8c44fd05d6474ab
danieldd57 <daniel.vargas19@uptc.edu.co>
2024-05-11 18:39:49 -0500

    functions #4, #5, #6 are implemented correctly

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* replaceOccurrencesChar(char*, char, char);
+int startWith(char*, char*);
+char* removeCharactersRepeated(char *);
+
 int main(void) {
     int option = 0;
+    char string [50];
+    char stringVerify [50];
+    char newChar;
+    char oldChar;
 
     while(option!=10){
-        printf("-----MENU----\n 4.Function #4 \n5.Function #5 \n6.Function #6 \n");
+        printf("\n-----MENU----\n4.Function #4 \n5.Function #5 \n6.Function #6 \n");
         scanf("%d", &option);
 
         switch (option) {
@@ -14,10 +25,30 @@ int main(void) {
             case 3:
                 break;
             case 4:
+                printf("Input string\n");
+                scanf(" %[^\n]", string);
+                printf("Input character to replace\n");
+                scanf(" %c", &oldChar);
+                printf("Input new character\n");
+                scanf(" %c", &newChar);
+                printf("%s", replaceOccurrencesChar(string, oldChar, newChar));
                 break;
             case 5:
+                printf("Input base string\n");
+                scanf(" %[^\n]", string);
+                printf("Input string to verify\n");
+                scanf(" %[^\n]", stringVerify);
+
+                if(startWith(string, stringVerify)==0){
+                    printf("The string to be verified (%s) NO exist!",stringVerify);
+                }else{
+                    printf("The string to be verified (%s) is t the start",stringVerify );
+                }
                 break;
             case 6:
+                printf("Input string\n");
+                scanf(" %[^\n]", string);
+                printf("%s", removeCharactersRepeated(string));
                 break;
             case 7:
                 break;
@@ -34,3 +65,62 @@ int main(void) {
     return 0;
 }
 
+char* removeCharactersRepeated(char *input) {
+    char *output = (char *)malloc(strlen(input) + 1);
+    int aux = 1;
+    output[0] = input[0];
+    //
+    int j;
+    for(int i=1; i<strlen(input); i++){
+        for(j=0; j<aux; j++){
+            if(output[j] == input[i]){
+                break; //for j
+            }
+        }
+        if (j==aux) {
+            output[aux] = input[i];
+            aux++;
+        }
+    }
+    output[aux] = '\0';
+    return output;
+}
+
+char* replaceOccurrencesChar(char *input, char oldChar, char newChar){
+    char *out = input;
+    for(int i=0; i<strlen(input); i++){
+        if(input[i] == oldChar){
+            out[i] = newChar;
+        }
+    }
+    return out;
+}
+
+/*
+ * char* removeCharactersRepeated(char *input){
+    int aux = 0;
+    char *output = (char *)malloc(strlen(input) + 1);
+
+    for(int i=0; i<strlen(input); i++){
+        if(strchr(output, input[i]) == NULL){
+            output[aux] = input[i];
+            aux++;
+        }
+
+    }
+    return output;
+}
+ */
+
+int startWith(char *baseString, char *verifyString){
+    if(strlen(verifyString) > strlen(baseString)){
+        return 0;
+    }else{
+        for(int i=0; i<strlen(verifyString); i++){
+            if(baseString[i] != verifyString[i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
