e6c0acb9ae35bec178b1e8eb907dcc058be664cc
danieldd57 <daniel.vargas19@uptc.edu.co>
2024-05-14 09:43:36 -0500

    merge functions 1-6

--- main.c
+++ main.c
@@ -49,7 +49,49 @@ char* substringClassic(char string[], int start, int end){
     return newString;
 }
 
+char* removeCharactersRepeated(char *input) {
+    char *output = (char *)malloc(strlen(input) + 1);
+    int aux = 1;
+    output[0] = input[0];
+    //
+    int j;
+    for(int i=1; i<strlen(input); i++){
+        for(j=0; j<aux; j++){
+            if(output[j] == input[i]){
+                break; //for j
+            }
+        }
+        if (j==aux) {
+            output[aux] = input[i];
+            aux++;
+        }
+    }
+    output[aux] = '\0';
+    return output;
+}
 
+char* replaceOccurrencesChar(char *input, char oldChar, char newChar){
+    char *out = input;
+    for(int i=0; i<strlen(input); i++){
+        if(input[i] == oldChar){
+            out[i] = newChar;
+        }
+    }
+    return out;
+}
+
+int startWith(char *baseString, char *verifyString){
+    if(strlen(verifyString) > strlen(baseString)){
+        return 0;
+    }else{
+        for(int i=0; i<strlen(verifyString); i++){
+            if(baseString[i] != verifyString[i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
 int main(void) {
     int option = 0;
     char string [50];
@@ -60,7 +102,7 @@ int main(void) {
     int endPosition;
 
     while(option!=10){
-        printf("\n-----MENU----\n1.Function #1 \n2.Function #2 \n3.Function #3 \n");
+        printf("\n-----MENU----\n1.Function #1 \n2.Function #2 \n3.Function #3 \n4.Function #4 \n5.Function #5 \n6.Function #6 \n");
         scanf("%d", &option);
 
         switch (option) {
@@ -106,12 +148,30 @@ int main(void) {
                 free(result3);
                 break;
             case 4:
-                
+                printf("Input string\n");
+                scanf(" %[^\n]", string);
+                printf("Input character to replace\n");
+                scanf(" %c", &oldChar);
+                printf("Input new character\n");
+                scanf(" %c", &newChar);
+                printf("%s", replaceOccurrencesChar(string, oldChar, newChar));
                 break;
             case 5:
-                
+                printf("Input base string\n");
+                scanf(" %[^\n]", string);
+                printf("Input string to verify\n");
+                scanf(" %[^\n]", stringVerify);
+
+                if(startWith(string, stringVerify)==0){
+                    printf("The string to be verified (%s) NO exist in the start!",stringVerify);
+                }else{
+                    printf("The string to be verified (%s) Is it the start",stringVerify );
+                }
                 break;
             case 6:
+                printf("Input string\n");
+                scanf(" %[^\n]", string);
+                printf("%s", removeCharactersRepeated(string));
                 break;
             case 7:
                 break;
@@ -120,9 +180,14 @@ int main(void) {
             case 9:
                 break;
             case 10:
+                printf("\n-- Exit --\n");
                 break;
+            default:
+                printf("\n-- Option invalid --\n");
+                break;
+
         }
-                printf("\n-- exit --\n");
+
     }
 
     return 0;
